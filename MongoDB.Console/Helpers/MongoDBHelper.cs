4a09464202027e2255b99a4b3e1cb88ee7c0cc71
thuraphyo <thuraphyo@mingalarinfotech.com>
2025-03-29 23:31:16 +0700

    add: added the basic CRUD operations by Mongo DB

---
+++ MongoDB.Console/Helpers/MongoDBHelper.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB.Console.Helpers
+{
+    public class MongoDBHelper
+    {
+        private readonly IMongoCollection<BsonDocument> _collection;
+
+        public MongoDBHelper(string connectionString, string dbName, string collectionName)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(dbName);
+            _collection = database.GetCollection<BsonDocument>(collectionName);
+        }
+
+        // Insert document
+        public async Task InsertAsync(BsonDocument document)
+        {
+            await _collection.InsertOneAsync(document);
+        }
+
+        // Read all document
+        public async Task<List<BsonDocument>> GetAllAsync()
+        {
+            return await _collection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        // Update document
+        public async Task<long> UpdateAsync(string id, BsonDocument document)
+        {
+            // Define the perdicate
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+
+            // Define the updated fields
+            var update = Builders<BsonDocument>.Update.Set("title", document["title"]);
+
+            // Update
+            var result = await _collection.UpdateOneAsync(filter, update);
+
+            return result.ModifiedCount;
+        }
+
+        public async Task<long> DeleteAsync(string id)
+        {
+            // Define the perdicate
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+
+            // Delete
+            var result = await _collection.DeleteOneAsync(filter);
+
+            return result.DeletedCount;
+        }
+    }
+}
---
+++ MongoDB.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson;
+using MongoDB.Console.Helpers;
+
+class Program()
+{
+    public static async Task Main()
+    {
+        string connectionString = "mongodb://localhost:27017";
+        string databaseName = "NewsDB";
+        string collectionName = "NewsArticles";
+
+        var mongoDbHelper = new MongoDBHelper(connectionString, databaseName, collectionName);
+
+        // Create the new document to create
+        var newsArticle = new BsonDocument
+        {
+            { "title" , "MongoDB News Integration"},
+            { "author" , "Thura Phyo"},
+            { "content" , "Learning MongoDB with .NET"},
+            { "published_date" , DateTime.Now},
+        };
+
+        await mongoDbHelper.InsertAsync(newsArticle);
+
+        var articles = await mongoDbHelper.GetAllAsync();
+        Console.WriteLine("News Articels \n");
+        foreach (var article in articles)
+        {
+            Console.WriteLine(article.ToJson());
+        }
+
+        if (articles != null && articles.Count > 0)
+        {
+            string? id = articles[0]["_id"]?.ToString();
+            var updateField = new BsonDocument { { "title", "Updated the title" } };
+            var updatedDocument = await mongoDbHelper.UpdateAsync(id!, updateField);
+
+            Console.WriteLine(updatedDocument > 0 ? "Document updated successfully." : "No document updated.");
+        }
+
+        if (articles != null && articles.Count > 0)
+        {
+            string? id = articles![0]["_id"].ToString();
+            var deletedDocument = await mongoDbHelper.DeleteAsync(id!);
+
+            if (deletedDocument > 0)
+                Console.WriteLine("Document deleted successfully.");
+            else
+                Console.WriteLine("No document found.");
+        }
+    }
+}
